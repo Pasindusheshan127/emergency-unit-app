93d2932374050b7bffbcd5bfa5bf44b4417f373d
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-12-18 15:09:55 +0530

    policeOfficer page is created

---
+++ client/src/pages/PoliceOfficerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PoliceOfficer from "../components/PoliceOfficer";
+
+const PoliceOfficerDashboard = () => {
+  return (
+    <div>
+      <PoliceOfficer officer={"Officer1"} id={"0123456789"} />
+    </div>
+  );
+};
+
+export default PoliceOfficerDashboard;
