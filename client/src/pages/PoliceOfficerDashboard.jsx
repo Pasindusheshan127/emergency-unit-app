eb9378b5300088fb9bbbfd79ebf0240412eccf5b
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-12-19 14:48:11 +0530

    PoliceOfiicerDashboard is updated

--- client/src/pages/PoliceOfficerDashboard.jsx
+++ client/src/pages/PoliceOfficerDashboard.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import PoliceOfficer from "./PoliceOfficer";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const PoliceOfficerDashboard = () => {
+  const [policeOfficers, setPoliceOfficers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/police-officers"
+        );
+        if (response.status === 200) {
+          setPoliceOfficers(response.data);
+          console.log(policeOfficers);
+        }
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+        alert("Failed to load data. Please try again.");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const officers = [
     { id: "0123456789", name: "Officer1" },
     { id: "2", name: "Officer2" },
@@ -13,10 +34,10 @@ const PoliceOfficerDashboard = () => {
     <div className="p-6 bg-white text-black">
       <h1 className="text-3xl font-bold text-center mb-16">Police Officers</h1>
       <div className="grid grid-cols-3 gap-4">
-        {officers.map((officer) => (
+        {policeOfficers.map((officer) => (
           <Link
             key={officer.id}
-            to={`/policeOfficers-dashboard/officer/${officer.id}`}
+            to={`/policeOfficers-dashboard/officer/${policeOfficers.id}`}
             className="px-6 py-3 bg-blue-500 text-white text-center rounded-lg hover:bg-blue-600"
           >
             {officer.name}
